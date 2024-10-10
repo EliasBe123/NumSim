6dd095af06377c8e83de8ec48876d322970ff30a
William Mårback <william.marback@hotmail.com>
2024-10-10 10:57:42 +0200

    SEIRDV model with solve_ivp

--- Projekt 2/epidemimodellering.py
+++ Projekt 2/epidemimodellering.py
@@ -6,9 +6,11 @@ alpha = 1
 beta = 0.3
 gamma = 1 / 7
 mu = 1 / 50
+v = 1 / 10
 
 n_pop = 1000
 
+v0 = 0
 d0 = 0
 r0 = 0
 i0 = 5
@@ -22,7 +24,10 @@ s0 = n_pop - i0
 # y0 = s0, e0, i0, r0
 
 # SEIRD model
-y0 = s0, e0, i0, r0, d0
+# y0 = s0, e0, i0, r0, d0
+
+# SEIRD model
+y0 = s0, e0, i0, r0, d0, v0
 
 t0, t1 = 0, 120
 tspan = t0, t1
@@ -60,6 +65,18 @@ def seird_ode(t, y):
 
     return [s_prime, e_prime, i_prime, r_prime, d_prime]
 
+def seirdv_ode(t, y):
+    s, e, i, r, d, v = y
+
+    s_prime = -beta * (i / n_pop) * s - v
+    e_prime = beta * (i / n_pop) * s - alpha * e
+    i_prime = alpha * e - gamma * i - mu * i
+    r_prime = gamma * i
+    d_prime = mu * i
+    v_prime = v
+
+    return [s_prime, e_prime, i_prime, r_prime, d_prime, v_prime]
+
 # SIR model
 # y = solve_ivp(sir_ode, tspan, y0, t_eval=teval)
 # plt.plot(y.t, y.y[0], label="s(t)")
@@ -74,12 +91,21 @@ def seird_ode(t, y):
 # plt.plot(y.t, y.y[3], label="r(t)")
 
 # SEIRD model
-y = solve_ivp(seird_ode, tspan, y0, t_eval=teval)
+# y = solve_ivp(seird_ode, tspan, y0, t_eval=teval)
+# plt.plot(y.t, y.y[0], label="s(t)")
+# plt.plot(y.t, y.y[1], label="e(t)")
+# plt.plot(y.t, y.y[2], label="i(t)")
+# plt.plot(y.t, y.y[3], label="r(t)")
+# plt.plot(y.t, y.y[4], label="d(t)")
+
+# SEIRDV model
+y = solve_ivp(seirdv_ode, tspan, y0, t_eval=teval)
 plt.plot(y.t, y.y[0], label="s(t)")
 plt.plot(y.t, y.y[1], label="e(t)")
 plt.plot(y.t, y.y[2], label="i(t)")
 plt.plot(y.t, y.y[3], label="r(t)")
 plt.plot(y.t, y.y[4], label="d(t)")
+plt.plot(y.t, y.y[5], label="v(t)")
 
 plt.title("Projekt 2")
 plt.grid()
