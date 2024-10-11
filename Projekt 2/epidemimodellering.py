801a35c5062bd554db2cd498b2fd8e7c48d72c61
William Mårback <william.marback@hotmail.com>
2024-10-11 15:42:49 +0200

    Changes to variable name to remain consistent across the code

--- Projekt 2/epidemimodellering.py
+++ Projekt 2/epidemimodellering.py
@@ -7,25 +7,26 @@ beta = 0.3
 gamma = 1 / 7
 mu = 1 /21
 vac = 7.5 / 10
-N = 1000
-n_pop = 1000
+
 v1 = 1/10
 v2 = 1/28
 im = 1/14
-E0= 5
 
-rv20 = 0
-rv10 = 0
-imr0 = 0
-im0 = 0
-v20 = 0
-v10 = 0
-v0 = 0
-d0 = 0
-r0 = 0
-i0 = 5
-e0 = 1 / 5
-s0 = n_pop - i0
+# E_0= 5
+
+N = 1000
+RV2_0 = 0
+RV1_0 = 0
+IMREC_0 = 0
+IM_0 = 0
+V2_0 = 0
+V1_0 = 0
+V_0 = 0
+D_0 = 0
+R_0 = 0
+I_0 = 5
+E_0 = 1 / 5
+S_0 = N - I_0
 
 
 
@@ -36,19 +37,19 @@ s0 = n_pop - i0
 
 
 # SIR model
-# sir0 = s0, i0, r0
+sir0 = S_0, I_0, R_0
 
 # SEIR model
-# seir0 = s0, e0, i0, r0
+seir0 = S_0, E_0, I_0, R_0
 
 # SEIRD model
-# seird0 = s0, e0, i0, r0, d0
+seird0 = S_0, E_0, I_0, R_0, D_0
 
 # SEIRDV model
-# seirdv0 = s0, e0, i0, r0, d0, v0
+seirdv0 = S_0, E_0, I_0, R_0, D_0, V_0
 
 # Own model
-own0 = s0, e0, i0, r0, d0, v10, v20, im0, imr0, rv10, rv20
+own0 = S_0, E_0, I_0, R_0, D_0, V1_0, V2_0, IM_0, IMREC_0, RV1_0, RV2_0
 
 t0, t1 = 0, 120
 tspan = t0, t1
@@ -57,61 +58,61 @@ h = 0.1
 teval = np.arange(t0, t1, h)
 
 def sir_ode(t, y):
-    s, i, r = y
+    S, I, R = y
 
-    s_prime = -beta * (i / n_pop) * s
-    i_prime = beta * (i / n_pop) * s - gamma * i
-    r_prime = gamma * i
+    S_prime = -beta * (I / N) * S
+    I_prime = beta * (I / N) * S - gamma * I
+    R_prime = gamma * I
 
-    return np.array([s_prime, i_prime, r_prime])
+    return np.array([S_prime, I_prime, R_prime])
 
 def seir_ode(t, y):
-    s, e, i, r = y
+    S, E, I, R = y
 
-    s_prime = -beta * (i / n_pop) * s
-    e_prime = beta * (i / n_pop) * s - alpha * e
-    i_prime = alpha * e - gamma * i
-    r_prime = gamma * i
+    S_prime = -beta * (I / N) * S
+    E_prime = beta * (I / N) * S - alpha * E
+    I_prime = alpha * E - gamma * I
+    R_prime = gamma * I
 
-    return np.array([s_prime, e_prime, i_prime, r_prime])
+    return np.array([S_prime, E_prime, I_prime, R_prime])
 
 def seird_ode(t, y):
-    s, e, i, r, d = y
+    S, E, I, R, D = y
 
-    s_prime = -beta * (i / n_pop) * s
-    e_prime = beta * (i / n_pop) * s - alpha * e
-    i_prime = alpha * e - gamma * i - mu * i
-    r_prime = gamma * i
-    d_prime = mu * i
+    S_prime = -beta * (I / N) * S
+    E_prime = beta * (I / N) * S - alpha * E
+    I_prime = alpha * E - gamma * I - mu * I
+    R_prime = gamma * I
+    D_prime = mu * I
 
-    return np.array([s_prime, e_prime, i_prime, r_prime, d_prime])
+    return np.array([S_prime, E_prime, I_prime, R_prime, D_prime])
 
 def seirdv_ode(t, y):
-    s, e, i, r, d, v = y
+    S, E, I, R, D, V = y
 
-    s_prime = -beta * (i / n_pop) * s - vac
-    e_prime = beta * (i / n_pop) * s - alpha * e
-    i_prime = alpha * e - gamma * i - mu * i
-    r_prime = gamma * i
-    d_prime = mu * i
-    v_prime = vac
+    S_prime = -beta * (I / N) * S - vac
+    E_prime = beta * (I / N) * S - alpha * E
+    I_prime = alpha * E - gamma * I - mu * I
+    R_prime = gamma * I
+    D_prime = mu * I
+    V_prime = vac
 
-    return np.array([s_prime, e_prime, i_prime, r_prime, d_prime, v_prime])
+    return np.array([S_prime, E_prime, I_prime, R_prime, D_prime, V_prime])
 
 def own_model_ode(t, y):
-    s, e, i, r, d, vac1, vac2, immune, immune_recovery, recovery_vac1, recovery_vac2 = y
+    S, E, I, R, D, V1, V2, IM, IM_REC, R_V1, R_V2 = y
     
-    exposure_rate = beta * s * (i / n_pop)
-    exposure_rate_v1 = (beta / 2) * vac1 * (i / n_pop)
-    infection_rate = alpha * e
-    recovery_rate = gamma * i
-    death_rate = mu * i
-    vaccination_rate_v1 = v1 * s
-    vaccination_rate_v2 = v2 * vac1
-    full_immunity_from_vaccination = im * vac2
-    full_immunity_from_recovery = im * r
-    recovery_to_v1 = v1 * r
-    recovery_to_v2 = v2 * r
+    exposure_rate = beta * S * I / N
+    exposure_rate_v1 = (beta / 2) * V1 * (I / N)
+    infection_rate = alpha * E
+    recovery_rate = gamma * I
+    death_rate = mu * I
+    vaccination_rate_v1 = v1 * S
+    vaccination_rate_v2 = v2 * V1
+    full_immunity_from_vaccination = im * V2
+    full_immunity_from_recovery = im * R
+    recovery_to_v1 = v1 * R
+    recovery_to_v2 = v2 * R
 
     return np.array([exposure_rate, exposure_rate_v1, infection_rate,
                      recovery_rate, death_rate, vaccination_rate_v1,
@@ -167,8 +168,6 @@ def propensities_sir(X, coeff):
     
     return np.array([infection_rate, recovery_rate])
 
-
-
 def propensities_seir(X, coeff):
     S, E, I, R = X  # Current state
     beta, gamma, alpha, N = coeff  # Model parameters: beta, gamma, N (total population)
@@ -176,7 +175,6 @@ def propensities_seir(X, coeff):
     expotion_rate = beta * S * I / N
     infection_rate = alpha * E- gamma * I
     recovery_rate = gamma * I
-    
     
     return np.array([expotion_rate, infection_rate, recovery_rate])
 
@@ -189,7 +187,6 @@ def propensities_seird(X, coeff):
     recovery_rate = gamma * I
     death_rate = mu * I
 
-    
     return np.array([exposure_rate, infection_rate, recovery_rate, death_rate])
 
 def propensities_seirdv(X, coeff):
@@ -323,7 +320,7 @@ def propensities_seirdv1v2im(X, coeff):
 #                           [0, 0, 0, -1, 0, 1, 0, 0],  #R -> V1
 #                            [0, 0, 0, -1, 0, 0, 1, 0] ]) #R -> V2
 
-# X0 = np.array([N - E0, 0, E0, 0, 0, 0, 0, 0])
+# X0 = np.array([N - E0, 0, 0, I0, 0, 0, 0, 0])
 
 # tspan = [0, 120] 
 
