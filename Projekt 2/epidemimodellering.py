4d0ddf9244435c0a962a73541b90cf6b44a4ffd9
William Mårback <william.marback@hotmail.com>
2024-10-11 15:32:09 +0200

    Very broken own model ODE

--- Projekt 2/epidemimodellering.py
+++ Projekt 2/epidemimodellering.py
@@ -13,6 +13,13 @@ v1 = 1/10
 v2 = 1/28
 im = 1/14
 E0= 5
+
+rv20 = 0
+rv10 = 0
+imr0 = 0
+im0 = 0
+v20 = 0
+v10 = 0
 v0 = 0
 d0 = 0
 r0 = 0
@@ -29,16 +36,19 @@ s0 = n_pop - i0
 
 
 # SIR model
-#y0 = s0, i0, r0
+# sir0 = s0, i0, r0
 
 # SEIR model
-#y0 = s0, e0, i0, r0
+# seir0 = s0, e0, i0, r0
 
 # SEIRD model
-# y0 = s0, e0, i0, r0, d0
+# seird0 = s0, e0, i0, r0, d0
 
-# SEIRD model
-#y0 = s0, e0, i0, r0, d0, v0
+# SEIRDV model
+# seirdv0 = s0, e0, i0, r0, d0, v0
+
+# Own model
+own0 = s0, e0, i0, r0, d0, v10, v20, im0, imr0, rv10, rv20
 
 t0, t1 = 0, 120
 tspan = t0, t1
@@ -53,7 +63,7 @@ def sir_ode(t, y):
     i_prime = beta * (i / n_pop) * s - gamma * i
     r_prime = gamma * i
 
-    return [s_prime, i_prime, r_prime]
+    return np.array([s_prime, i_prime, r_prime])
 
 def seir_ode(t, y):
     s, e, i, r = y
@@ -63,7 +73,7 @@ def seir_ode(t, y):
     i_prime = alpha * e - gamma * i
     r_prime = gamma * i
 
-    return [s_prime, e_prime, i_prime, r_prime]
+    return np.array([s_prime, e_prime, i_prime, r_prime])
 
 def seird_ode(t, y):
     s, e, i, r, d = y
@@ -74,7 +84,7 @@ def seird_ode(t, y):
     r_prime = gamma * i
     d_prime = mu * i
 
-    return [s_prime, e_prime, i_prime, r_prime, d_prime]
+    return np.array([s_prime, e_prime, i_prime, r_prime, d_prime])
 
 def seirdv_ode(t, y):
     s, e, i, r, d, v = y
@@ -86,7 +96,27 @@ def seirdv_ode(t, y):
     d_prime = mu * i
     v_prime = vac
 
-    return [s_prime, e_prime, i_prime, r_prime, d_prime, v_prime]
+    return np.array([s_prime, e_prime, i_prime, r_prime, d_prime, v_prime])
+
+def own_model_ode(t, y):
+    s, e, i, r, d, vac1, vac2, immune, immune_recovery, recovery_vac1, recovery_vac2 = y
+    
+    exposure_rate = beta * s * (i / n_pop)
+    exposure_rate_v1 = (beta / 2) * vac1 * (i / n_pop)
+    infection_rate = alpha * e
+    recovery_rate = gamma * i
+    death_rate = mu * i
+    vaccination_rate_v1 = v1 * s
+    vaccination_rate_v2 = v2 * vac1
+    full_immunity_from_vaccination = im * vac2
+    full_immunity_from_recovery = im * r
+    recovery_to_v1 = v1 * r
+    recovery_to_v2 = v2 * r
+
+    return np.array([exposure_rate, exposure_rate_v1, infection_rate,
+                     recovery_rate, death_rate, vaccination_rate_v1,
+                     vaccination_rate_v2, full_immunity_from_vaccination,
+                     full_immunity_from_recovery, recovery_to_v1, recovery_to_v2])
 
 def SSA(prop, stoch, X0, tspan, coeff):
 
@@ -177,7 +207,7 @@ def propensities_seirdv(X, coeff):
     
 def propensities_seirdv1v2im(X, coeff):
     S, E, I, R, D, V1, V2, IM = X  # Current state
-    beta, gamma, alpha, mu, v1, v2,im, N = coeff  # Model parameters
+    beta, gamma, alpha, mu, v1, v2, im, N = coeff  # Model parameters
     
     # Calculate propensities (reaction rates)
     exposure_rate = beta * S * I / N       # S -> E
@@ -200,20 +230,22 @@ def propensities_seirdv1v2im(X, coeff):
 
 # SIR model
 
-stochiometry = np.array([[-1, 1, 0],  
-                        [0, -1, 1]])
+# stochiometry = np.array([[-1, 1, 0],  
+#                         [0, -1, 1]])
 
-X0 = np.array([995, 5, 0])
+# X0 = np.array([995, 5, 0])
 
-tspan = [0, 120] 
+# tspan = [0, 120] 
 
-coeff = [0.3, 1/7, 1000]
-tvec, Xarr = SSA(propensities_sir, stochiometry, X0, tspan, coeff)
+# coeff = [0.3, 1/7, 1000]
+# tvec, Xarr = SSA(propensities_sir, stochiometry, X0, tspan, coeff)
 
 
-plt.plot(tvec, Xarr[:, 0], label='Susceptible')
-plt.plot(tvec, Xarr[:, 1], label='Infected')
-plt.plot(tvec, Xarr[:, 2], label='Recovered')
+# plt.plot(tvec, Xarr[:, 0], label='Susceptible')
+# plt.plot(tvec, Xarr[:, 1], label='Infected')
+# plt.plot(tvec, Xarr[:, 2], label='Recovered')
+
+
 
 
 
@@ -315,6 +347,22 @@ plt.plot(tvec, Xarr[:, 2], label='Recovered')
 #     result += Xarr[:][-1][4]
 
 # res = result/1
+
+
+y = solve_ivp(fun=own_model_ode, t_span=tspan, y0=own0, t_eval=teval)
+
+plt.plot(y.t, y.y[0], label='Susceptible')
+plt.plot(y.t, y.y[1], label='Exposed')
+plt.plot(y.t, y.y[2], label='Infected')
+plt.plot(y.t, y.y[3], label='Recovered')
+plt.plot(y.t, y.y[4], label='Dead')
+plt.plot(y.t, y.y[5], label='Vaccinated1')
+plt.plot(y.t, y.y[6], label='Vaccinated2')
+plt.plot(y.t, y.y[7], label='Immune')
+plt.plot(y.t, y.y[8], label='Immune recovery')
+plt.plot(y.t, y.y[9], label='Recovery vaccinated 1')
+plt.plot(y.t, y.y[10], label='Recovery vaccinated 2')
+
 plt.title("Projekt 2")
 plt.grid()
 plt.xlabel('Time (days)')
