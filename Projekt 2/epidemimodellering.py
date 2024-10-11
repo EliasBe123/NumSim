e12ceb350440d1f48d7d35bcca4ae491481e4c88
William Mårback <william.marback@hotmail.com>
2024-10-11 17:36:50 +0200

    Working ODE of SEIRDV1V2IM
    Reduced to not use all graphs due to clutter and not being that important to show

--- Projekt 2/epidemimodellering.py
+++ Projekt 2/epidemimodellering.py
@@ -42,8 +42,8 @@ SEIRD_0 = np.array([S_0, E_0, I_0, R_0, D_0])
 SEIRDV_0 = np.array([S_0, E_0, I_0, R_0, D_0, V_0])
 
 # Own model
-SEIRDV1V2IM_0_ode = np.array([S_0, E_0, I_0, R_0, D_0, V1_0, V2_0, IM_0, IMREC_0, RV1_0, RV2_0])
-SEIRDV1V2IM_0_stoch = np.array([S_0, E_0, I_0, R_0, D_0, V1_0, V2_0, IM_0])
+SEIRDV1V2IM_0_full = np.array([S_0, E_0, I_0, R_0, D_0, V1_0, V2_0, IM_0, IMREC_0, RV1_0, RV2_0])
+SEIRDV1V2IM_0_light = np.array([S_0, E_0, I_0, R_0, D_0, V1_0, V2_0, IM_0])
 
 t0, t1 = 0, 120
 tspan = t0, t1
@@ -93,25 +93,24 @@ def seirdv_ode(t, y):
 
     return np.array([S_prime, E_prime, I_prime, R_prime, D_prime, V_prime])
 
-def own_model_ode(t, y):
-    S, E, I, R, D, V1, V2, IM, IM_REC, R_V1, R_V2 = y
-    
-    exposure_rate = beta * S * I / N
-    exposure_rate_v1 = (beta / 2) * V1 * (I / N)
-    infection_rate = alpha * E
-    recovery_rate = gamma * I
-    death_rate = mu * I
-    vaccination_rate_v1 = v1 * S
-    vaccination_rate_v2 = v2 * V1
-    full_immunity_from_vaccination = im * V2
-    full_immunity_from_recovery = im * R
-    recovery_to_v1 = v1 * R
-    recovery_to_v2 = v2 * R
+def seirdv1v2im_ode(t, y):
+    # S, E, I, R, D, V1, V2, IM, IM_REC, R_V1, R_V2 = y
+    S, E, I, R, D, V1, V2, IM = y
 
-    return np.array([exposure_rate, exposure_rate_v1, infection_rate,
-                     recovery_rate, death_rate, vaccination_rate_v1,
-                     vaccination_rate_v2, full_immunity_from_vaccination,
-                     full_immunity_from_recovery, recovery_to_v1, recovery_to_v2])
+    S_prime = -beta * (S * I) / N - v1 * S
+    E_prime = beta * (S * I) / N + (beta / 2) * (V1 * I) / N - alpha * E
+    I_prime = alpha * E - gamma * I - mu * I
+    R_prime = gamma * I - v1 * R - v2 * R - im * R
+    D_prime = mu * I
+    V1_prime = v1 * S - v2 * V1 - (beta / 2) * (V1 * I) / N
+    V2_prime = v2 * V1 - im * V2
+    IM_prime = im * V2
+    IM_REC_prime = im * R
+    R_V1_prime = v1 * R
+    R_V2_prime = v2 * R
+
+    # return np.array([S_prime, E_prime, I_prime, R_prime, D_prime, V1_prime, V2_prime, IM_prime, IM_REC_prime, R_V1_prime, R_V2_prime])
+    return np.array([S_prime, E_prime, I_prime, R_prime, D_prime, V1_prime, V2_prime, IM_prime])
 
 def SSA(prop, stoch, X0, tspan, coeff):
 
@@ -225,7 +224,7 @@ def propensities_seirdv1v2im(X, coeff):
 
 
 # ODE
-ode = -1
+ode = 4
 
 # SIR
 if ode == 0:
@@ -267,7 +266,7 @@ elif ode == 3:
 
 # Own (SEIRDV1V2IM)
 elif ode == 4:
-    y = solve_ivp(fun=own_model_ode, t_span=tspan, y0=SEIRDV1V2IM_0_ode, t_eval=teval)
+    y = solve_ivp(fun=seirdv1v2im_ode, t_span=tspan, y0=SEIRDV1V2IM_0_light, t_eval=teval)
 
     plt.plot(y.t, y.y[0], label='Susceptible')
     plt.plot(y.t, y.y[1], label='Exposed')
@@ -277,9 +276,9 @@ elif ode == 4:
     plt.plot(y.t, y.y[5], label='Vaccinated1')
     plt.plot(y.t, y.y[6], label='Vaccinated2')
     plt.plot(y.t, y.y[7], label='Immune')
-    plt.plot(y.t, y.y[8], label='Immune recovery')
-    plt.plot(y.t, y.y[9], label='Recovery vaccinated 1')
-    plt.plot(y.t, y.y[10], label='Recovery vaccinated 2')
+    # plt.plot(y.t, y.y[8], label='Immune recovery')
+    # plt.plot(y.t, y.y[9], label='Recovery vaccinated 1')
+    # plt.plot(y.t, y.y[10], label='Recovery vaccinated 2')
 
 
 stoch = 4
@@ -355,15 +354,13 @@ elif stoch == 4:
                              [ 0, 0, 0,-1, 0, 1, 0, 0],  #R -> V1
                              [ 0, 0, 0,-1, 0, 0, 1, 0]]) #R -> V2
 
-    tvec, Xarr = SSA(propensities_seirdv1v2im, stochiometry, SEIRDV1V2IM_0_stoch, tspan, coeff)
+    tvec, Xarr = SSA(propensities_seirdv1v2im, stochiometry, SEIRDV1V2IM_0_light, tspan, coeff)
 
     plt.plot(tvec, Xarr[:, 0], label='Susceptible')
     plt.plot(tvec, Xarr[:, 1], label='Exposed')
     plt.plot(tvec, Xarr[:, 2], label='Infected')
     plt.plot(tvec, Xarr[:, 3], label='Recovered')
     plt.plot(tvec, Xarr[:, 4], label='Dead')
-    plt.plot(tvec, Xarr[:, 5], label='Vaccinated1')
-    plt.plot(tvec, Xarr[:, 6], label='Vaccinated2')
     plt.plot(tvec, Xarr[:, 7], label='Immune')
 
 # i = 0
